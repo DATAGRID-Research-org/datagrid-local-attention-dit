7d5c1a8cf8b2c1f5e560133e889c4eeeb7171f50
Yohan Lee <alien2327@gmail.com>
2025-05-17 08:30:32 +0900

    Add pipeline base class

--- localdit/pipeline_i2i.py
+++ localdit/pipeline_i2i.py
@@ -1,2 +1,10 @@
 from diffusers import PixArtAlphaPipeline
 from diffusers.models.attention import Attention
+
+class LocalDiTImg2ImgPipeline(PixArtAlphaPipeline):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
+    
